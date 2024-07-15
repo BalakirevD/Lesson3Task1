f18ff43ee098a31e427571e6af606cb1ba73d092
Balakirev <b1ackhead@yandex.ru>
2024-07-15 23:05:48 +0300

    Add BonusMileService2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public int calculate (int price) {
+    public int calculate(int price) {
         int mileCoast = 20;
         int miles = price / mileCoast;
 
